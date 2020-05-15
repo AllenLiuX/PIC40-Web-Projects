827a991f5ec182dabcb414c5c93eb9d14cbc889a
VincentLiu <13120200491@163.com>
2020-05-15 17:19:41 +0800

    cookie done

--- projects/HW4/cookieGreeter.js
+++ projects/HW4/cookieGreeter.js
@@ -1,14 +1,23 @@
+
+var moveId = 0;
+var changeId = 0;
+var direction = 1;
+var name;
+var c_speed = 0;
+var c_color = 'red';
+
 function greet() {
-    let name = prompt("What is your name?", "");
-    var valid = false;
+    name = prompt("What is your name?", "");
+    get_cookie();
+    let valid = false;
     $.ajax({
         url: "important.txt",
         datatype: "text",
         success: function (data) {
-            // $("h1").html(data);
             let names = data.split('\n');
-            for (n of names) {
-                n = n.substr(0, n.length-1);
+            for (let n of names) {
+                if (n !== names[names.length-1])
+                    n = n.substr(0, n.length-1);
                 if (name === n){
                     valid = true;
                 }
@@ -32,26 +41,101 @@ function greet() {
                 $("#color").append("<label for='blue'> blue </label>");
                 $("#color").append("<input type='radio' id='blue' value='blue' onclick='update()' name='Color'>");
                 $("main").append("<div id='back'><div id='rectangle'>Welcome "+name+"</div></div>");
+
+
+                $("#p"+c_speed).attr("checked", true);
+                // $("main").append("<h1>"+c_color+"</h1>");
+                $("#"+c_color).attr("checked", true);
+
+                $("#back").css("width", "100%");
+                $("#back").css("height", "300px");
+                $("#back").css("background-color","grey");
                 $("#rectangle").css("background-color", "red");
                 $("#rectangle").css("text-align", "center");
-                $("#rectangle").css("height", "100px");
-                $("#rectangle").css("width", "200px");
-                $("#rectangle").css("margin-top", "40px");
-                $("#rectangle").css("position","relative");
-                $("#back").css("width", "100vw");
-                $("#back").css("height", "30vh");
-                $("#back").css("background-color","grey");
+                $("#rectangle").css("height", "150px");
+                $("#rectangle").css("width", "300px");
+                $("#rectangle").css("margin-top", "75px");
+                $("#rectangle").css("position","absolute");
             }
         }
     });
+    // if(c_speed>=0)
+    update();
 }
 
 function update() {
-    let cur_color = $('input[name=Color]:checked').val();
-    $('main').append("<h3>"+cur_color+"</h3>");
-    $("#rectangle").css("background-color", cur_color);
-    // $('#rectangle').css("transition", "all 2s ease");
-    $('#rectangle').css("float", "right");
+    // let cur_color = $('input[name=Color]:checked').val();
+    // $("#rectangle").css("background-color", cur_color);
+    if(changeId !== 0)
+        clearInterval(changeId);
+    changeId = setInterval(change, 1000);   //check the options every second
+
+    if(moveId!==0)
+        clearInterval(moveId);
+    moveId=setInterval(move, 10);   //update the animation every 10 msec
+}
+
+function change() {
+    make_cookie();
+    c_color = $('input[name=Color]:checked').val();
+    c_speed = Number($('input[name=Speed]:checked').val());
+}
+
+function move() {
+    // if(c_color !== "")
+    $("#rectangle").css("background-color", c_color);
+    let cur_left = $("#rectangle").position().left;
+    let cur_speed = c_speed/2;
+    if(isNaN(cur_left))    // something === NaN is always evaluated as false
+        cur_left=0;
+    else
+        cur_left = Number(cur_left);
+    if(cur_left>window.innerWidth-307)
+        direction = -1;
+    else if(cur_left-cur_speed<8)
+        direction = 1;
+    let new_left = cur_left+cur_speed*direction;
+    // $("main").append("<h1>"+cur_left+"</h1>");
+    $("#rectangle").css("left", new_left+"px");
+}
 
 
+function make_cookie(){
+    let cookie_name = "name=" + name + ";";
+    let user_speed = $('input[name=Speed]:checked').val();
+    let cookie_speed = "speed=" + user_speed + ";";
+    let user_color = $('input[name=Color]:checked').val();
+    let cookie_color = "color="+user_color+";";
+    let now = new Date(), expires = now;
+    expires.setSeconds(expires.getSeconds()+20);
+    let cookie_expires = "expires=" + expires.toUTCString() + ";";
+    let cookie_path = "path=" + window.location.pathname;
+    document.cookie = cookie_name + cookie_expires + cookie_path;
+    document.cookie = cookie_speed + cookie_expires + cookie_path;
+    document.cookie = cookie_color + cookie_expires + cookie_path;
+}
+
+
+function get_cookie() {
+    let cookie = document.cookie.split(';');
+    let name_matched = false;
+    for(let part of cookie){    //check whether the name matches the name in the cookie
+        let pieces = part.split('=');
+        pieces[0] = pieces[0].trim();
+        if (pieces[0] === 'name' && pieces[1] === name){
+            name_matched = true;
+        }
+    }
+    if (name_matched){
+        for(let part of cookie){
+            let pieces = part.split('=');
+            pieces[0] = pieces[0].trim();
+            if (pieces[0] === 'speed'){
+                c_speed = Number(pieces[1].trim());
+            }
+            else if (pieces[0] === 'color'){
+                c_color = pieces[1].trim();
+            }
+        }
+    }
 }
